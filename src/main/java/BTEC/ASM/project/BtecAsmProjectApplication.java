7c8a7416535b3e3c8a56743ea11881fe71f1ab33
John Doe <tauhaitac3k@gmail.com>
2026-01-05 11:53:10 +0700

    Initial commit

---
+++ src/main/java/BTEC/ASM/project/BtecAsmProjectApplication.java
@@ -0,0 +1,13 @@
+package BTEC.ASM.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtecAsmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtecAsmProjectApplication.class, args);
+	}
+
+}
