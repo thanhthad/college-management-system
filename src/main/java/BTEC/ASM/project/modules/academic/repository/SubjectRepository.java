f8b95b5975c2c2ea397eafdef0bc0fafc36f1059
John Doe <tauhaitac3k@gmail.com>
2026-02-22 21:04:31 +0700

    Feat(subject): add queries in repository

--- src/main/java/BTEC/ASM/project/modules/academic/repository/SubjectRepository.java
+++ src/main/java/BTEC/ASM/project/modules/academic/repository/SubjectRepository.java
@@ -1,5 +1,8 @@
 package BTEC.ASM.project.modules.academic.repository;
 
+import BTEC.ASM.project.modules.academic.entity.Term;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +16,14 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     Optional<Subject> findBySubjectCode(String subjectCode);
 
     boolean existsBySubjectCode(String subjectCode);
+
+    boolean existsBySubjectCodeAndSubjectIdNot(String subjectCode, Long subjectId);
+
+    Page<Subject> findBySubjectNameContainingIgnoreCase(
+            String subjectName,
+            Pageable pageable
+    );
+
+    Page<Subject> findAll(Pageable pageable);
+
 }
