fc591a7fa170d6c17e83457ac33aecb7d958a26d
John Doe <tauhaitac3k@gmail.com>
2026-03-10 22:01:25 +0700

    feat(enrollment): add enrollment files response and mapper

---
+++ src/main/java/BTEC/ASM/project/modules/enrollment/dto/response/EnrollmentResponse.java
@@ -0,0 +1,23 @@
+package BTEC.ASM.project.modules.enrollment.dto.response;
+
+import java.time.LocalDateTime;
+
+public class EnrollmentResponse {
+
+    private String studentCode;
+    private String studentName;
+
+    private String subjectCode;
+    private String subjectName;
+
+    private String termCode;
+    private String termName;
+
+    private String classGroupName;
+    private String classGroupCode;
+
+    private String enrollStatus;
+
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/BTEC/ASM/project/modules/enrollment/mapper/EnrollmentMapper.java
@@ -0,0 +1,36 @@
+package BTEC.ASM.project.modules.enrollment.mapper;
+
+import BTEC.ASM.project.modules.enrollment.dto.request.EnrollmentRequest;
+import BTEC.ASM.project.modules.enrollment.dto.response.EnrollmentResponse;
+import BTEC.ASM.project.modules.enrollment.entity.Enrollment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface EnrollmentMapper {
+
+    @Mapping(source = "offeringId", target = "offering.id")
+    @Mapping(source = "studentUserId", target = "student.id")
+    Enrollment toEntity(EnrollmentRequest request);
+
+
+    @Mapping(source = "student.userCode", target = "studentCode")
+    @Mapping(source = "student.fullName", target = "studentName")
+    @Mapping(source = "offering.subject.subjectCode", target = "subjectCode")
+    @Mapping(source = "offering.subject.subjectName", target = "subjectName")
+    @Mapping(source = "offering.term.termCode", target = "termCode")
+    @Mapping(source = "offering.term.termName", target = "termCode")
+    @Mapping(source = "offering.classGroup.groupName", target = "classGroupName")
+    @Mapping(source = "offering.classGroup.campusCode", target = "classGroupCode")
+    EnrollmentResponse toResponse(Enrollment enrollment);
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "offeringId", target = "offering.id")
+    @Mapping(source = "studentUserId", target = "student.id")
+    void updateEnrollmentFromRequest(
+            EnrollmentRequest request,
+            @MappingTarget Enrollment entity
+    );
+}
