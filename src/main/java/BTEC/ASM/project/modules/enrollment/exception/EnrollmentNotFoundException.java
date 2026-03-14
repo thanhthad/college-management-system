a4b0b61dd02c06d94fead39719c332dd0ff593b6
John Doe <tauhaitac3k@gmail.com>
2026-03-14 21:41:19 +0700

    feat(enrollment): add exceptions

---
+++ src/main/java/BTEC/ASM/project/modules/enrollment/exception/EnrollmentNotFoundException.java
@@ -0,0 +1,7 @@
+package BTEC.ASM.project.modules.enrollment.exception;
+
+public class EnrollmentNotFoundException extends RuntimeException {
+    public EnrollmentNotFoundException(String message) {
+        super(message);
+    }
+}
